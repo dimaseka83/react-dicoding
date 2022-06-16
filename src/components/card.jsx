761fb49177fae14a5bfc1e02e02df81b0d847c33
dhimaseka <dimaseka83@gmail.com>
2022-06-16 16:56:43 +0700

    ++

---
+++ src/components/card.jsx
@@ -0,0 +1,41 @@
+import style from "../styles/card.module.css";
+import {
+    showFormattedDate
+} from "../utils"
+import Button from "./button";
+
+const Card = ({
+    id,
+    title,
+    createdAt,
+    body,
+    archived,
+    action
+}) => {
+    const deleteNote = (item) => action((notes) => notes.filter(note => note.id !== item.id));
+    const toggleArchive = (item) => action((notes) => notes.map(note => {
+        if (note.id === item) {
+            return {
+                ...note,
+                archived: !note.archived
+            };
+        }
+        return note;
+    }));
+
+    return (
+        <article id="{id}" className="{style.card}">
+            <header className="{style.card_header}">
+                <h3 className="{style.heading}">{title}</h3>
+                <small className="{style.date}">{showFormattedDate(createdAt)}</small>
+                <p className="{style.note}">{body}</p>
+            </header>
+            <footer className="{style.card_footer}">
+                <Button eventHandler={() => deleteNote(id)} label="Delete" />
+                <Button eventHandler={() => toggleArchive(id)} label={archived ? "Unarchive" : "Archive"} />
+            </footer>
+        </article>
+    )
+}
+
+export default Card;
